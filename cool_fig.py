5e96d1fe7ff8310bdd21c5ed5321cc87f9d08f71
cghiaus <72642214+cghiaus@users.noreply.github.com>
2021-10-19 16:10:27 +0200

    Add files via upload

---
+++ cool_fig.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 15:59:35 2021
+
+@author: cghiaus
+
+Figures for the paper
+C. Ghiaus (2021) Computational psychrometric analysis of cooling systems as
+a control problem: case of cooling and dehumidification systems,
+International Journal of Building Performance Simulation,
+DOI: 10.1080/19401493.2021.1995498
+"""
+import ipywidgets as wd
+import matplotlib.pyplot as plt
+import cool as cc
+
+# %matplotlib inline  # uncomment for inline figure
+# %matplotlib qt      # uncomment for figure in separate window
+plt.rcParams["figure.figsize"] = (10, 7.7)
+font = {'size': 16}
+plt.rc('font', **font)
+
+Kθ, Kw = 1e10, 0        # Gain factors of the P-controllers
+β = 0.16                # By-pass factor of the cooling coil
+
+m, mo = 3.1, 1.         # kg/s, mass flow rate, supply and outdoor air
+θo, φo = 32., 0.5       # °C, -, outdoor air temperature and relative humidity
+θ5sp, φ5sp = 26., 0.5   # °C, -, indoor air set points
+
+mi = 1.35               # kg/s, mass flow rate of infiltration air
+UA = 675.               # W/K, overall heat transfet coeffcient
+QsBL, QlBL = 34000., 4000.    # W, sensible & latent auxiliar heat
+
+parameters = m, mo, β, Kθ, Kw
+inputs = θo, φo, θ5sp, φ5sp, mi, UA, QsBL, QlBL
+
+def no_6():
+    cool0 = cc.MxCcRhTzBl(parameters, inputs)
+    Kw = 1e10
+    cool0.actual[4] = Kw
+    wd.interact(cool0.CAV_wd, θo=(26, 34), φo=(0.4, 1), θ5sp=(20, 28), φ5sp=(0.30, 1, 0.01),
+                mi=(0.1, 3, 0.1), UA=(500, 800, 10), QsBL=(0, 60_000, 500), QlBL=(0, 20_000, 500))
+    print("CAV systems with reheating")
+    print("Control of indoor air temperature & humidity (θ5, φ5)")
